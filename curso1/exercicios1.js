0589a5b776f858edff4fb363735e8119490f4d20
Leticia <leticia.delchello@gmail.com>
2025-03-29 09:01:52 -0300

    Realizando exercicios da aula 1 do curso

---
+++ curso1/exercicios1.js
@@ -0,0 +1,35 @@
+// Exercicio 1
+alert("Boas vindas ao nosso site!");
+
+// Exercicio 2
+let nome = "Lua";
+
+// Exercicio 3
+let idade = 25;
+
+// Exercicio 4
+let numeroDeVendas = 50;
+
+// Exercicio 5
+let saldoDisponivel = 1000;
+
+// Exercicio 6
+alert("Erro! Preencha todos os campos");
+
+// Exercicio 7
+let mensagemDeErro = "Erro! Preencha todos os campos";
+alert(mensagemDeErro);
+
+// Exercicio 8
+let nome1 = prompt("Digite a seguir o seu nome: ");
+
+// Exercicio 9
+let idade1 = prompt("Digite a seguir a sua idade: ");
+
+// Exercicio 10
+let nome2 = prompt("Digite a seguir o seu nome: ");
+let idade2 = prompt("Digite a seguir a sua idade: ");
+
+if (idade >= 18){
+	alert("Pode tirar a habilitação!");
+}

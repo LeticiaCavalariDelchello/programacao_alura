f0c9b0619b04db56aa8c3d2f02cc38f6a5ac88c9
Leticia <leticia.delchello@gmail.com>
2025-04-08 23:08:16 -0300

    Realizando os exercícios da aula 4 do curso 2

---
+++ curso2_desafio_4/app.js
@@ -0,0 +1,21 @@
+// Exercício 1
+let listaGenerica = [];
+
+// Exercício 2
+let linguagensDeProgramacao = ['JavaScript' , 'C' , 'C++' , 'Kotlin' , 'Python'];
+
+// Exercício 3
+let linguagensDeProgramacao1 = ['JavaScript' , 'C' , 'C++' , 'Kotlin' , 'Python'];
+linguagensDeProgramacao1.push('Java' , 'Ruby' , 'GoLang');
+
+// Exercício 4
+let nomes = ['Leticia' , 'Maria' , 'Luiza'];
+console.log(nomes[0]);
+
+// Exercício 5
+let frutas = ['Maçã' , 'Morango' , 'Goiaba'];
+console.log(frutas[1]);
+
+// Exercício 6
+let carros = ['BMW - 320i' , 'Rampage' , 'BMW - X1'];
+console.log(carros[2]);
